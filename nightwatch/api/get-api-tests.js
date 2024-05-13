920906ce15aadc7ba15084bef35b634c3245be36
rubb3rDucc <mingos1337@gmail.com>
2024-05-12 23:25:47 -0400

    completed first test and was succesful

--- nightwatch/api/get-api-tests.js
+++ nightwatch/api/get-api-tests.js
@@ -1,20 +1,26 @@
-describe('api testing', function () {
-    it('get api test', async function ({ supertest }) {
+// describe('api testing', function () {
+//     it('get api test', async function ({ supertest }) {
+//         await supertest
+//             .request("https://petstore.swagger.io/v2")
+//             .get("/pet/findByStatus?status=available")
+//             .expect(200)
+//             .expect('Content-Type', /json/)
+//             .then(function (response) {
+//                 expect(response._body.length).to.be.greaterThan(0);
+//             });
+//     });
+// });
+
+describe('test', function () {
+    it('gets all todos', async function ({ supertest }) {
         await supertest
-            .request("https://petstore.swagger.io/v2")
-            .get("/pet/findByStatus?status=available")
+            .request("http://localhost:3000/")
+            .get('todo/byUserId')
+            .send({ userId: '231' })
             .expect(200)
             .expect('Content-Type', /json/)
             .then(function (response) {
                 expect(response._body.length).to.be.greaterThan(0);
             });
-    });
-});
-
-describe('test', function () {
-    it('get all todos', async function ({ supertest }) {
-        await supertest
-        .request("http://localhost:3000/todo/byUserId")
-            .expect(200)
     })
 });
